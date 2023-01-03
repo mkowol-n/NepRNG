ca0d390785f068392e6b185bf72378ce567fb637
mKowol <mateuszkowoln@gmail.com>
2023-01-03 21:36:58 +0100

    add test

---
+++ app/src/test/java/pl/nepapp/neprng/SecondTest.kt
@@ -0,0 +1,11 @@
+package pl.nepapp.neprng
+
+import org.junit.Assert
+import org.junit.Test
+
+class SecondTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(5, 2 + 3)
+    }
+}
